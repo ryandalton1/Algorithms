45a5365d8ff4fc29e28d39c51994d094de1679df
Ryan Dalton <43825386+ryandalton1@users.noreply.github.com>
2020-11-03 13:59:40 -0700

    Add files via upload

---
+++ Bredth-First Search/Q2_starter_code.py
@@ -0,0 +1,167 @@
+"""
+@author: Rhoenigman
+         Shivendra
+"""
+import networkx as nx
+
+"""
+The function to generate the input graph
+
+:return: Returns the NetworkX Graph for Q2
+"""
+
+
+def Question2():
+    # Create a directed graph
+    G = nx.DiGraph()
+
+    # The 'length' on each edge should be ignored and is only for drawing.
+    # Adding an edge also adds the node
+    G.add_edge('EC', 'A', length=40, weight=1.0)
+    G.add_edge('EC', 'H', length=40, weight=1.0)
+    G.add_edge('EC', 'J', length=60, weight=1.0)
+
+    G.add_edge('H', 'G', length=40, weight=1.0)
+    G.add_edge('H', 'K', length=40, weight=1.0)
+
+    G.add_edge('G', 'L', length=40, weight=1.0)
+    G.add_edge('G', 'F', length=40, weight=1.0)
+
+    G.add_edge('F', 'E', length=40, weight=1.0)
+
+    G.add_edge('E', 'HUMN', length=40, weight=1.0)
+
+    G.add_edge('J', 'S', length=80, weight=1.0)
+    G.add_edge('J', 'K', length=60, weight=1.0)
+
+    G.add_edge('K', 'L', length=40, weight=1.0)
+    G.add_edge('L', 'M', length=40, weight=1.0)
+    G.add_edge('M', 'N', length=40, weight=1.0)
+    G.add_edge('M', 'F', length=60, weight=1.0)
+
+    G.add_edge('N', 'O', length=80, weight=1.0)
+    G.add_edge('N', 'E', length=80, weight=1.0)
+
+    G.add_edge('O', 'HUMN', length=40, weight=1.0)
+
+    G.add_edge('A', 'S', length=60, weight=1.0)
+    G.add_edge('A', 'B', length=40, weight=1.0)
+
+    G.add_edge('B', 'R', length=40, weight=1.0)
+    G.add_edge('B', 'C', length=40, weight=1.0)
+
+    G.add_edge('S', 'R', length=60, weight=1.0)
+    G.add_edge('R', 'Q', length=40, weight=1.0)
+
+    G.add_edge('Q', 'C', length=40, weight=1.0)
+    G.add_edge('Q', 'P', length=60, weight=1.0)
+
+    G.add_edge('C', 'D', length=40, weight=1.0)
+    G.add_edge('D', 'HUMN', length=40, weight=1.0)
+    G.add_edge('P', 'D', length=40, weight=1.0)
+    G.add_edge('P', 'O', length=60, weight=1.0)
+    G.add_edge('O', 'HUMN', length=40, weight=1.0)
+
+    G.add_edge('T', 'Q', length=40, weight=1.0)
+    G.add_edge('T', 'P', length=40, weight=1.0)
+    G.add_edge('T', 'O', length=40, weight=1.0)
+    G.add_edge('T', 'N', length=40, weight=1.0)
+    G.add_edge('T', 'M', length=40, weight=1.0)
+
+    G.add_edge('R', 'T', length=40, weight=1.0)
+    G.add_edge('S', 'T', length=40, weight=1.0)
+    G.add_edge('J', 'T', length=40, weight=1.0)
+    G.add_edge('K', 'T', length=40, weight=1.0)
+    G.add_edge('L', 'T', length=40, weight=1.0)
+
+    return G
+
+
+"""
+A utility function to help visualize the generated graph
+
+:param G: NetworkX Graph
+:return: None (instead saves the input graph in .png format)
+"""
+
+
+def draw_graph(G):
+    import matplotlib.pyplot as plt
+    import pylab
+    edge_labels = dict([((u, v,), d['weight'])
+                        for u, v, d in G.edges(data=True)])
+    node_labels = {node: node for node in G.nodes()}
+
+    pos = nx.spectral_layout(G)
+    nx.draw_networkx_labels(G, pos, labels=node_labels)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    nx.draw(G, pos, node_size=500, edge_cmap=plt.cm.Reds)
+    plt.savefig('Finals_Q2_Graph.png')
+    pylab.title("Input Graph")
+    pylab.show()
+
+
+solutionSet = []
+
+
+def recursive(graph, u, v, k, path):
+    global solutionSet
+    # the point of this function is to take in a node
+    # and create a path, checking all its neighbor nodes
+    # if it reaches the end and k = 0 then good
+    path.append(u)
+    if u == v and k == 0:
+        # if you go in here, you've followed a path that is a success
+        solutionSet.append(path)
+
+    # go through all neighbors, add them to the path then follow that neighbor
+    for x in list(graph[u]):
+        b = []
+        for y in path:
+            b.append(y)
+        recursive(graph, x, v, k - 1, b)
+
+def helperFunction(G, u, v, k):
+    global solutionSet
+    p = []
+    k += 1
+    recursive(G, u, v, k, p)
+    # at this point solutionSet is filled with all the possible paths
+    # G['EC']['A']['weight'] would equal 1.0
+
+    pathLengths = []
+    for x in solutionSet:
+        counter = 0
+        for y in range(len(x)-1):
+            counter += G[x[y]][x[y+1]]['weight']
+        pathLengths.append(counter)
+    index = pathLengths.index(min(pathLengths))
+    return solutionSet[index]
+
+
+
+def main():
+    global solutionSet
+    ################## READ CAREFULLY ##############################
+
+    # Note that you cannot use any networkx functionality
+    # which makes the solution trivial
+
+    # The 'length' on each edge (while generating the graph)
+    # should be ignored and is only for drawing.
+    # You should consider the 'weight' for finding the smallest path.
+    # The above example has weights 1 but the weight can be anything.
+    # Later on we may post some more graphs for testing.
+    G = Question2()
+    draw_graph(G)
+
+    # Call your function here that takes in the Graph "G"
+    # and returns the shortest path
+    # (note that it is not the length but the entire path)
+
+    print(helperFunction(G, 'EC', 'HUMN', 4)) # when k is 3 should only have one path
+
+
+if __name__ == "__main__":
+    # The driver function
+    main()
